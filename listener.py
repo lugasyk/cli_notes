4bf6503541fc59a3c0bb9974747eb969314205c4
lugasyk <lugassyk@gmail.com>
2018-09-17 22:28:59 +0200

    start to implement task #2, task #3 and task #4

--- listener.py
+++ listener.py
@@ -1,7 +1,20 @@
+notes = {}
+
 def listener():
-    listener_str = raw_input('Enter command:')
-    if listener_str == "Echo":
-        print ('You use right command!')
+    firstcom_str = 'Add'
+    secondcom_str = 'List'
+    thirdcom_str = 'Show'
+    print (firstcom_str + ' or ' + secondcom_str + ' or ' + thirdcom_str)
+    listener_str = raw_input('Enter command: ')
+    if listener_str.startswith('Add'):
+        index = len(notes)
+        notes[index] = listener_str
+        print(notes[index])
+        print str.replace("You've inter:", " ", )
+    elif listener_str == "List":
+        print notes
+    elif listener_str == "Show":
+        print
     else:
         print ("Ooops, something went wrong...")
 
