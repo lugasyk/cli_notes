78f85b0d2b4f2a80dd4f5fe177c8fed1356a13f5
lugasyk <lugassyk@gmail.com>
2018-10-20 21:57:22 +0200

    done tasks #3 and task #4

--- listener.py
+++ listener.py
@@ -1,22 +1,26 @@
+from webbrowser import get
+
 notes = {}
+
 
 def listener():
     firstcom_str = 'Add'
     secondcom_str = 'List'
     thirdcom_str = 'Show'
     print (firstcom_str + ' or ' + secondcom_str + ' or ' + thirdcom_str)
-    listener_str = raw_input('Enter command: ')
-    if listener_str.startswith('Add'):
-        index = len(notes)
-        notes[index] = listener_str
-        print(notes[index])
-        print str.replace("You've inter:", " ", )
-    elif listener_str == "List":
-        print notes
-    elif listener_str == "Show":
-        print
-    else:
-        print ("Ooops, something went wrong...")
+    while True:
+        listener_str = raw_input('Enter command: ')
+        if listener_str.startswith('Add'):
+            index = len(notes)
+            listener_str = raw_input('')
+            notes[index] = listener_str
+            print(notes[index])
+        elif listener_str == "List":
+            print notes
+        elif listener_str.startswith('Show'):
+            notes.get(index)
+        else:
+            print ("Ooops, something went wrong...")
 
 
 if __name__ == "__main__":
