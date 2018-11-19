c49f35d5447deff1ca2a4f6e5ec9e55325c9fbf6
lugasyk <lugassyk@gmail.com>
2018-11-19 21:38:50 +0100

    done tasks #7

--- listener.py
+++ listener.py
@@ -6,7 +6,8 @@ def listener():
     firstcom_str = 'Add'
     secondcom_str = 'List'
     thirdcom_str = 'Show'
-    print (firstcom_str + ' or ' + secondcom_str + ' or ' + thirdcom_str)
+    fourth_str = 'Help'
+    print (firstcom_str + ' or ' + secondcom_str + ' or ' + thirdcom_str + ' or ' + fourth_str)
     while True:
         listener_str = raw_input('Enter command: ')
         if listener_str.startswith('Add'):
@@ -23,6 +24,18 @@ def listener():
             index = int(raw_input(''))
             text = raw_input('')
             notes[index] = text
+        elif listener_str.startswith('Help'):
+            command = raw_input('')
+            if command == 'Add':
+                print ("This command helps you to add new notes.")
+            elif command == 'Show':
+                print ("This command shows the text of a note by an index.")
+            elif command == 'List':
+                print ("This command shows the list of notes with indexes.")
+            elif command == 'Update':
+                print ("This command updates the text of a note by an index.")
+            elif command == 'Help':
+                print ("This command shows the description of the each command.")
         else:
             print ("Ooops, something went wrong...")
 
